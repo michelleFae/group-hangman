1857912c40ed5f03c10a6431c7772f7d90ec1b5d
michelleFae <michelled@berkeley.edu>
2025-10-21 20:16:24 +0100

    more words

--- src/data/animals.js
+++ src/data/animals.js
@@ -5,6 +5,7 @@
 // and plural forms (including common irregular plurals).
 const BASE_ANIMALS = [
   'aardvark','alligator','ant','anteater','antelope','ape','armadillo','baboon','badger','bat','bear','beaver','bee','bison','boar','buffalo','butterfly','camel','canary','capybara','caribou','cat','caterpillar','cheetah','chicken','chimpanzee','chinchilla','cobra','cod','cougar','cow','coyote','crab','crow','deer','dingo','dog','dolphin','donkey','dove','duck','eagle','eel','elephant','elk','emu','falcon','ferret','fish','flamingo','fox','frog','gazelle','gerbil','giraffe','goat','goose','gorilla','grasshopper','grouse','guanaco','guinea','gull','hamster','hare','hawk','hedgehog','hippo', 'hippopotamus','hornet','horse','hummingbird','hyena','ibex','ibis','iguana','impala','jaguar','jay','kakapo','kangaroo','koala','krill','lemur','leopard','lion','lizard','llama','lobster','lynx','macaw','mole','monkey','moose','mosquito','moth','mouse','mule','narwhal','newt','nightingale','ocean','ocelot','octopus','opossum','orangutan','orca','ostrich','otter','owl','ox','oyster','panda','panther','parrot','peacock','pelican','penguin','pheasant','pigeon','porcupine','porpoise','possum','puma','quail','rabbit','raccoon','rat','raven','reindeer','rhinoceros','rook','salamander','salmon','sandpiper','saola','scorpion','seahorse','seal','shark','sheep','shrimp','skunk','sloth','snail','snake','sparrow','spider','squid','squirrel','starfish','stingray','stork','swan','tapir','tarsier','termite','tiger','toad','tortoise','trout','turkey','turtle','vole','vulture','walrus','wasp','weasel','whale','wolf','wombat','woodpecker','yak','zebra',
+  'addax','vaquita','condor',
 
   // common dog breeds (single-word, lowercase)
   'beagle','bulldog','dachshund','doberman','husky','poodle','chihuahua','pug','rottweiler','boxer','labrador','mastiff','greyhound','collie','shiba','akita','terrier',
@@ -22,6 +23,8 @@ const BASE_ANIMALS = [
 
   // common insect/bug names
   'cockroach','roach','beetle','ladybird','ladybug','mantis','cicada','dragonfly','damselfly','firefly','lightningbug','earwig','silverfish','centipede','millipede','aphid','weevil','grasshopper','lice',
+  // worm-related additions
+  'worm','earthworm','mealworm','silkworm',
 
   // plural forms previously present here have been removed so BASE_ANIMALS
   // contains only singular/base forms; plural forms are generated below.
@@ -46,11 +49,13 @@ const BASE_ANIMALS = [
 
   // extinct animals / prehistoric species
   'dodo','auroch','moa','passengerpigeon','woollymammoth','smilodon','mastodon','glyptodon','thylacine','quagga',
+  'mammoth',
 
   // round-2 extra animals
   'alpaca','oryx','dikdik','ayeaye','tenrec','coelacanth','cuttlefish','urchin','gecko','python','viper','locust','magpie','toucan','marmoset','howler','ram','gander',
   // added per request
   'zebrafish','zebu','zorse','zonkey', 'zooplankton'
+  , 'honeybadger','pangolin','axolotl'
 ]
 
 // A small set of well-known irregular plurals and no-change plurals
